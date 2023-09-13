8270fa75f84ccc9b5bafe2884687f970f87c5016
Adam Walters <122646712+Adamb0lt@users.noreply.github.com>
2023-09-13 01:03:54 -0400

    Update TweetWindow.java

--- TweetWindow.java
+++ TweetWindow.java
@@ -1,14 +1,15 @@
 /***************************************************************************
-* Author: Adam Walters
-* Date: May 8, 2023
-*
-* This program creates a GUI through JavaFX that reads out information on Tweets from the MakeTweetsArray class. 
-* It keeps the same functionality that would have been in the original MakeTweetsArray in
-* terms of what is needed as input and output for within the original MakeTweetsArray.
-* The same functionality is carried through.
-***************************************************************************/
-
+ * Author: Adam Walters
+ * Date: May 8, 2023
+ *
+ * This program creates a GUI through JavaFX that reads out information on Tweets
+ * from the MakeTweetsArray class. It keeps the same functionality that would
+ * have been in the original MakeTweetsArray in terms of what is needed as input
+ * and output for within the original MakeTweetsArray. The same functionality is
+ * carried through.
+ ***************************************************************************/
 package TweetsProj;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -19,20 +20,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-
 public class TweetWindow extends Application {
-    private Tweets tweets;// for accessing the tweets
-    private Label message, blank1; //messages and what will be the blank text
-    private TextField indexChoice;//textfield for user to choose index of tweet 
-    private TextArea details;//Textarea to display everything about the tweet chosen
-    private Button search, clear, exit; //buttons that perfom actions 
+    private Tweets tweets; // Reference to Tweets class for accessing Tweet information
+    private Label message, blank1; // Labels for messages and spacing
+    private TextField indexChoice; // Text field for user to input the index of the Tweet
+    private TextArea details; // Text area to display Tweet details
+    private Button search, clear, exit; // Buttons for performing actions
 
     public void start(Stage stage) throws Exception {
-        
-                
         GridPane root = new GridPane();
 
-        // Set up elements like buttons and labels
+        // Set up GUI elements
         Label choice = new Label("Enter the Index of Tweet:");
         indexChoice = new TextField();
         search = new Button("Search");
@@ -42,7 +40,7 @@ public class TweetWindow extends Application {
         message = new Label("");
         blank1 = new Label("");
 
-        // Add positions to each of the different buttons, labels, etc...
+        // Add elements to the layout grid
         root.add(choice, 0, 0);
         root.add(indexChoice, 1, 0);
         root.add(search, 2, 0);
@@ -52,56 +50,59 @@ public class TweetWindow extends Application {
         root.add(message, 0, 3, 3, 1);
         root.add(blank1, 0, 4, 3, 1);
 
-        // Set up button actions that are based on methods below 
+        // Set up button actions based on methods below
         search.setOnAction(e -> enterChoice());
         clear.setOnAction(e -> clearText());
         exit.setOnAction(e -> Platform.exit());
 
-        // Set up scene and show stage
+        // Set up the scene and display the stage
         Scene scene = new Scene(root, 500, 250);
         stage.setScene(scene);
         stage.setTitle("TweetWindow");
         stage.setResizable(false);
         stage.show();
-        
-        // Load tweets
-        MakeTweetsArray.load();
-        
 
+        // Load tweets from MakeTweetsArray
+        MakeTweetsArray.load();
     }
 
     private void enterChoice() {
-    // gives default blank text to any previous messages and details from textfield
-    double num1;
-    message.setText("");
-    details.setText("");
-    try {
-        num1 = Double.parseDouble(indexChoice.getText()); // user must enter a number. Index used for checking in if and else statements
-        int index = (int) num1; // also if they place a decimal, the decimal will be dropped and turned to an integer value
-        if (index < 0) {
-            message.setText("Please enter a positive integer for the index."); //if index number is not positive
-        } else if (index >= MakeTweetsArray.TweetObjects.length) {//if choice of index is greater than or equal to length, then this error message pops up instructing user on what to enter
-            message.setText("Index out of bounds. Please enter a valid index from 0 to "
-                    + (MakeTweetsArray.TweetObjects.length - 1) + ".");
-        } else if (MakeTweetsArray.TweetObjects[index] == null) {// if tweet object is null
-            message.setText("No valid tweet found at index " + index);
-        } else {// if index number is positive and in range of "length-1", then it outputs the information in textArea
-            details.setText(MakeTweetsArray.TweetObjects[index].toString());
-        }
-    } catch (NumberFormatException e) {//if user doesnt enter a number at all
-        message.setText("Please enter a valid number.");
-        indexChoice.selectAll();
-        indexChoice.requestFocus();
-    }
-}
+        // Reset any previous messages and clear text from the text field and area
+        double num1;
+        message.setText("");
+        details.setText("");
 
-    private void clearText() {//clears both the textfield and areafield and places in blanktext
+        try {
+            num1 = Double.parseDouble(indexChoice.getText()); // Parse the user's input as a number
+            int index = (int) num1; // Convert the number to an integer (drops decimals if present)
+
+            if (index < 0) {
+                message.setText("Please enter a positive integer for the index.");
+            } else if (index >= MakeTweetsArray.TweetObjects.length) {
+                message.setText("Index out of bounds. Please enter a valid index from 0 to "
+                        + (MakeTweetsArray.TweetObjects.length - 1) + ".");
+            } else if (MakeTweetsArray.TweetObjects[index] == null) {
+                message.setText("No valid tweet found at index " + index);
+            } else {
+                // Display the Tweet information in the text area
+                details.setText(MakeTweetsArray.TweetObjects[index].toString());
+            }
+        } catch (NumberFormatException e) {
+            message.setText("Please enter a valid number.");
+            indexChoice.selectAll();
+            indexChoice.requestFocus();
+        }
+    }
+
+    private void clearText() {
+        // Clear the text field and text area, and reset the blank text label
         indexChoice.clear();
         details.clear();
         blank1.setText("");
     }
 
-    public static void main(String[] args) {//for launching GUI
+    public static void main(String[] args) {
+        // Launch the GUI application
         launch();
     }
-} 
+}
